626013c9d46dc6df7337578a58b2b47aa8feb1e2
Will Chang <william.chaang@gmail.com>
2023-08-08 15:10:11 -0600

    Fancy view all table working.

---
+++ lib/main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+# imports
+from sqlalchemy import (create_engine, Column, Integer, String)
+from sqlalchemy.orm import (declarative_base, sessionmaker)
+from password_generator import PasswordGenerator
+from password_storage import PasswordStorage
+
+Base = declarative_base()
+
+
+if __name__ == '__main__':
+        engine = create_engine('sqlite:///passwords.db')
+        Base.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+                
+        def mainMenu(): 
+                print('Welcome to the Python Password Protector. What would you like to do?')
+                action = input("Create or Manage: ")
+
+                if action.lower() == "manage":
+                        print('Would you like to view or update logins?')
+                        action = input("View or Update: ")
+                        
+                        if action.lower() == "view":
+                                #Function to print all of the logins
+                                # all_logins = session.query(PasswordStorage)
+                                # print([login for login in all_logins])
+                                PasswordStorage.showAll()
+                        
+                        if action.lower() == "update":
+                                print('What platform login would you like to update?')
+                                edit_login = input('Enter platform: ')
+                                platform = session.query(PasswordStorage).filter(PasswordStorage.platform)
+                                username = session.query(PasswordStorage).filter(PasswordStorage.username)
+                                print(f'You have selected the login for {username} on {platform}')
+
+                                #Thinking update options will be to Regenerate password or Delete login 
+                                if action.lower() == "Regenerate password":
+                                        pass
+
+                                if action.lower() == "Delete password":
+                                        to_delete = session.query(PasswordStorage).filter(PasswordStorage.id.like({edit_id}))
+
+
+                        print("... \n"
+                        "... \n"
+                        "... \n")
+                        mainMenu()
+
+                elif action.lower() == "create":
+                        new_password = PasswordGenerator().password_gen()
+
+                        session.add(new_password)
+                        session.commit() 
+                        
+                        print("... \n"
+                        "... \n"
+                        "... \n")
+                        mainMenu()
+
+
+mainMenu()
--- lib/password_storage.py
+++ lib/password_storage.py
@@ -1,5 +1,8 @@
 from sqlalchemy import (create_engine, Column, Integer, String)
 from sqlalchemy.orm import (declarative_base, sessionmaker)
+from texttable import Texttable
+
+
 
 Base = declarative_base()
 
@@ -13,3 +16,32 @@ class PasswordStorage(Base):
 
         def __repr__(self): 
                 return f"Username: {self.username} Password: {self.password} Platform: {self.platform}"
+        
+        def showAll():
+                # standard command for URL call assigned to engine to be used in the next line
+                engine = create_engine('sqlite:///passwords.db')
+
+                # generates required SQL data from stored data in Base class metadata
+                Base.metadata.create_all(engine)
+
+                # creates sessionmaker class instance on the engine variable connecting them so we can execute queries in the correct location
+                Session = sessionmaker(bind=engine)
+
+                # creates session object that will be used to execute queries in the correct location
+                session = Session()
+
+                # this executes the query in the correct location and returns the results in list form
+                data = session.query(PasswordStorage).all()
+
+                # creates TextTable object that will be used to display results in an easy to read format
+                textTable = Texttable()
+
+                # creates TextTable headers that our data will be displayed under
+                textTable.header(['Username', 'Password', 'Platform'])
+
+                # loop that will create a new row from each username and password to be displayed
+                for datum in data:
+                        textTable.add_row([datum.username, datum.password, datum.platform])
+
+                # print textTable with draw function to calculate then draw the table based on the data it is given
+                print(textTable.draw()) 
